7ad2f07677e89d22f875b19febc0ee8012cd4351
josuegoyret <fjgoyretsola@gmail.com>
2024-05-10 16:32:52 -0300

    fix(day-page): log env vars

--- sbd-client/lib/config.ts
+++ sbd-client/lib/config.ts
@@ -4,3 +4,5 @@ export const APOD_API_URL = 'https://api.nasa.gov/planetary/apod';
 export const YEAR = 2024;
 
 export const API_URL = process.env.API_URL;
+
+console.log({ APOD_API_KEY, API_URL });
