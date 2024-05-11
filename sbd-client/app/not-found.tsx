fca38058de909c14bfa28b5c70af1963ae623ff2
josuegoyret <fjgoyretsola@gmail.com>
2024-05-11 15:57:26 -0300

    feat: not found page

--- sbd-client/app/not-found.tsx
+++ sbd-client/app/not-found.tsx
@@ -5,15 +5,17 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 // eslint-disable-next-line
-const DayPageError = ({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) => {
+const NotFoundPage = () => {
   return (
     <main className='w-full h-screen relative flex flex-col gap-10 items-center '>
       <Image src={NASA_BOUNDRY_PHOTO} alt='The Earth photo' fill className='object-cover -z-50' priority />
       <div className='w-full h-full px-2 md:px-7 pt-10 pb-48 hero-gradient text-nasa-white flex flex-col justify-center'>
-        <div className='px-4 sm:px-7 md:px-10 flex flex-col gap-16 w-full max-w-screen-2xl mx-auto'>
-          <h1 className='text-center font-label text-7xl md:text-9xl font-light'>500</h1>
+        <div className='px-4 sm:px-7 md:px-10 flex flex-col gap-10 md:gap-16 w-full max-w-screen-2xl mx-auto'>
+          <h1 className='text-center font-label text-7xl md:text-9xl font-light'>404</h1>
           <div className='flex flex-col items-center justify-center gap-6'>
-            <p className='max-w-xl text-xl md:text-2xl text-center'>{`It's been a problem while getting your information: seems like this APOD is not available. Check the URL and try again or return to calendar.`}</p>
+            <p className='max-w-xl text-xl md:text-2xl text-center'>
+              The cosmic object you were looking for has disappeared beyond the event horizon.
+            </p>
             <div className='flex item-center justify-center w-max gap-2 md:gap-4'>
               <Link
                 className='text-lg text-nasa-white font-medium p-2 blue-square cursor-pointer hover:underline decoration-dashed underline-offset-8'
@@ -22,13 +24,13 @@ const DayPageError = ({ error, reset }: { error: Error & { digest?: string }; re
               >
                 Return Calendar
               </Link>
-              <button
-                onClick={() => reset()}
+              <Link
                 className='text-lg text-nasa-white font-medium p-2 blue-square cursor-pointer hover:underline decoration-dashed underline-offset-8'
-                aria-label='Try again button'
+                aria-label='Navigate to day page'
+                href={'/calendar/day'}
               >
-                Try Again
-              </button>
+                Day Page
+              </Link>
             </div>
           </div>
         </div>
@@ -37,4 +39,4 @@ const DayPageError = ({ error, reset }: { error: Error & { digest?: string }; re
   );
 };
 
-export default DayPageError;
+export default NotFoundPage;
